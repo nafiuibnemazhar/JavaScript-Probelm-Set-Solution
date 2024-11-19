5d6e8c3e1814a2ef747b7dbc807bfd5daf5d9a5e
nafiuibnemazhar <159128338+nafiuibnemazhar@users.noreply.github.com>
2024-11-19 09:02:49 +0600

    first commit

---
+++ currentDayTime.js
@@ -0,0 +1,9 @@
+/**
+ * Write a JavaScript program to display the current day and time in the following format.
+Today is : Tuesday.
+Current time is : 10 PM : 30 : 38
+
+This JavaScript program retrieves the current date and time, determines the day of the week, and formats the current hour, minute, and second in 12-hour format with AM/PM notation. It then prints the day of the week and the formatted current time to the console. The program also handles special cases for "Noon" and "Midnight" to ensure an accurate representation.
+ */
+
+const today = new Date();

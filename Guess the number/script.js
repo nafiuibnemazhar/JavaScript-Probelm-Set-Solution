0dfe9fa52dbbec10334fa96e9ed4d4e5f2eb6cf6
nafiuibnemazhar <159128338+nafiuibnemazhar@users.noreply.github.com>
2024-11-20 09:33:49 +0600

    updated

--- Guess the number/script.js
+++ Guess the number/script.js
@@ -1,3 +1,7 @@
+/**
+ * Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. The program displays a message "Good Work" if the input matches the guess number otherwise "Not matched".
+ */
+
 let num = Math.ceil(Math.random() * 10);
 
 console.log(num);

e5617dd56ca8d4b3781f5606d241b410bb7897c9
nafiuibnemazhar <159128338+nafiuibnemazhar@users.noreply.github.com>
2024-11-22 12:46:04 +0600

    solved

---
+++ CalculateMultiplyDivide/main.js
@@ -0,0 +1,12 @@
+function multiplyBy() {
+  number1 = document.getElementById("firstNumber").value;
+  number2 = document.getElementById("secondNumber").value;
+
+  document.getElementById("result").innerHTML = number1 * number2;
+}
+function divideBy() {
+  number1 = document.getElementById("firstNumber").value;
+  number2 = document.getElementById("secondNumber").value;
+
+  document.getElementById("result").innerHTML = number1 / number2;
+}
